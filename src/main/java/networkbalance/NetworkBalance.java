c01ea3c74d2dfe1b7d8b8c144e10184171a95950
Thomas Froud <thomasfroud10@gmail.com>
2025-07-24 13:02:14 +0100

    Initial project creation

---
+++ src/main/java/networkbalance/NetworkBalance.java
@@ -0,0 +1,17 @@
+package networkbalance;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NetworkBalance extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
