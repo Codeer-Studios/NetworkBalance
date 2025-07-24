687dbd54c1113bd8c319af0a76a1fd8818629b4a
Thomas Froud <thomasfroud10@gmail.com>
2025-07-24 19:53:30 +0100

    Removed un-needed stuff

--- src/main/java/networkbalance/NetworkBalance.java
+++ src/main/java/networkbalance/NetworkBalance.java
@@ -3,35 +3,22 @@ package networkbalance;
 import com.google.gson.Gson;
 import networkbalance.commands.*;
 import networkbalance.database.DatabaseManager;
-import networkbalance.messaging.PluginMessageHandler;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.Map;
 
 public final class NetworkBalance extends JavaPlugin {
 
     private static NetworkBalance instance;
     private DatabaseManager databaseManager;
-    private PluginMessageHandler messageHandler;
-    private Gson gson;
 
     @Override
     public void onEnable() {
         instance = this;
         saveDefaultConfig();
-
-        gson = new Gson();
         databaseManager = new DatabaseManager(this);
-        messageHandler = new PluginMessageHandler(this, databaseManager, gson);
-
-        getServer().getMessenger().registerIncomingPluginChannel(this, "network:core", messageHandler);
-        getServer().getMessenger().registerOutgoingPluginChannel(this, "network:core");
-
-        getLogger().info("Registering plugin channels on network:core");
-
 
         getCommand("balance").setExecutor(new BalanceCommand(databaseManager));
         getCommand("balancegive").setExecutor(new BalanceGiveCommand(databaseManager));
@@ -47,15 +34,6 @@ public final class NetworkBalance extends JavaPlugin {
             databaseManager.shutdown();
         }
         getLogger().info("NetworkBalance disabled.");
-    }
-
-    public void sendPluginMessage(Player player, Map<String, Object> data) {
-        String json = gson.toJson(data); // reuse existing Gson
-
-        getLogger().info("[sendPluginMessage] Sending plugin message to player " + player.getName() + ": " + json);
-
-        byte[] payload = json.getBytes(StandardCharsets.UTF_8);
-        player.sendPluginMessage(this, "network:core", payload);
     }
 
     public static NetworkBalance getInstance() {
